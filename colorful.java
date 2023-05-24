e92cfe6aed4bbea2483f35262388016b32ed6ee5
Martin Uzunov <96121194+MartinUzunuv@users.noreply.github.com>
2023-05-24 20:54:10 +0300

    Update colorful.java

--- colorful.java
+++ colorful.java
@@ -1,7 +1,18 @@
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class colorful {
+class pixel{
+    public double x;
+    public double y;
+    public String color;
+    public pixel(double x, double y, String color){
+        this.x = x;
+        this.y = y;
+        this.color = color;
+    }
+}
+
+public class az {
     public static final String reset = "\u001B[0m";    //return to default color
     public static final String black = "\u001B[30m";   //choose color
     public static final String red = "\u001B[31m";
@@ -20,30 +31,46 @@ public class colorful {
         System.out.println(s);
     }
 
+    public static pixel[][] arr = new pixel[160][12];
+
+    public static void fill(int startX, int startY, int endX, int endY, String color){
+        for(int x = startX; x < endX; x++){
+            for(int y = startY; y < endY; y++){
+                arr[x][y] = new pixel(x,y,color);
+            }
+        }
+    }
+
     // we want to print a matrix with objects like {string:"#", color: red}
 
     public static void main(String[] args){
         //syntax: p(red+"az");
-        int maxX = 160;
-        int maxY = 12;
+        int maxX = arr.length;
+        int maxY = arr[0].length;
         String field[][] = new String[maxX][maxY];
+        fill(0,0,160,12,green);
+        fill(10,2,20,5,red);
+        fill(30,4,60,10,blue);
         Timer timer = new Timer();
         TimerTask task = new TimerTask() {
             double n = 0;
             @Override
             public void run() {  //this function will be repeated as frames
-//                System.out.print("\033[H\033[2J");
-//                System.out.flush();
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
                 for (double y = 0; y < maxY; y++) {
                     for (double x = 0; x < maxX; x++) {
+                        int intX = (int) x;
+                        int intY = (int) y;
                         String element = "";
-                        element += red; //color
-//                        if (x + y > n) {
+//                        element += red; //color
+////                        if (x + y > n) {
+////                            element += blue;
+////                        }
+//                        if(Math.sin(x/4+n)*3+5>y){
 //                            element += blue;
 //                        }
-                        if(Math.sin(x/4+n)*3+5>y){
-                            element += blue;
-                        }
+                        element+= arr[intX][intY].color;
                         element += "@"; //string (one element recommended)
                         element += reset;
                         p(element);
@@ -56,4 +83,4 @@ public class colorful {
 
         };
         timer.scheduleAtFixedRate(task, 0, 500);
-}}
+    }}
