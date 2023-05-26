052bae6e84e744f35450d293eb3e2794a01c4073
Martin Uzunov <96121194+MartinUzunuv@users.noreply.github.com>
2023-05-26 23:26:37 +0300

    Alpha update 1

--- colorful.java
+++ colorful.java
@@ -1,18 +1,7 @@
 import java.util.Timer;
 import java.util.TimerTask;
 
-class pixel{
-    public double x;
-    public double y;
-    public String color;
-    public pixel(double x, double y, String color){
-        this.x = x;
-        this.y = y;
-        this.color = color;
-    }
-}
-
-public class az {
+public class colorfull {
     public static final String reset = "\u001B[0m";    //return to default color
     public static final String black = "\u001B[30m";   //choose color
     public static final String red = "\u001B[31m";
@@ -32,13 +21,13 @@ public class az {
     }
 
     //FIELD SIZE:
-    public static pixel[][] arr = new pixel[160][12];
+    public static String[][] arr = new String[160][12];
 
     public static void fill(int startX, int startY, int endX, int endY, String color){
         for(int x = startX; x < endX; x++){
             for(int y = startY; y < endY; y++){
                 if(x<arr.length && y<arr[1].length&&x>=0&&y>=0){
-                    arr[x][y] = new pixel(x,y,color);
+                    arr[x][y] = color;
                 }
             }
         }
@@ -68,21 +57,26 @@ public class az {
         }
     }
 
-    // we want to print a matrix with objects like {string:"#", color: red}
-
     public static void main(String[] args){
         //syntax: p(red+"az");
         int maxX = arr.length;
         int maxY = arr[0].length;
-        String field[][] = new String[maxX][maxY];
         fill(0,0,arr.length,arr[1].length,white);
         double maxXB = (double) arr.length/2;
         double maxYB = (double) arr[0].length;
         Timer timer = new Timer();
         TimerTask task = new TimerTask() {
-            //INIT-------------------------------------------------------------------------------------------------------->
-            double n = 0;
-            //------------------------------------------------------------------------------------------------------------>
+            //INIT--------------------------------------------------------------------------------------------------------------------->
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            //------------------------------------------------------------------------------------------------------------------------->
             @Override
             public void run() {  //this function will be repeated as frames
                 System.out.print("\033[H\033[2J");
@@ -94,7 +88,7 @@ public class az {
                         int intX = (int) x;
                         int intY = (int) y;
                         String element = "";
-                        element+= arr[intX][intY].color;
+                        element+= arr[intX][intY];
                         element += "@"; //string (one element recommended)
                         element += reset;
                         p(element);
@@ -103,16 +97,18 @@ public class az {
                 }
                 //background color
                 fill(0,0,maxX,maxY,black);
-                //UPDATE & DRAW------------------------------------------------------------------------------------------->
+                //UPDATE & DRAW-------------------------------------------------------------------------------------------------------->
 
-                for(double a = 0; a < Math.PI*50; a+=Math.PI/400){
-                    fillB(Math.cos(a+n)*0.5*a+30, Math.sin(a+n)*0.5*a+7,1, 1, blue);
-                }
+                
+                
+                
+                
+                
+                
+                
 
-                n+=0.3;
 
-
-                //-------------------------------------------------------------------------------------------------------->
+                //--------------------------------------------------------------------------------------------------------------------->
             }
 
 
